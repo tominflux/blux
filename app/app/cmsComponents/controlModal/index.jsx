a85d952a54302827dd750e7e8b471927c3317276
Tom <tom.chesters96@gmail.com>
2020-05-28 18:03:04 +0100

    Remove media-browser and page-browser

--- app/app/cmsComponents/controlModal/index.jsx
+++ app/app/cmsComponents/controlModal/index.jsx
@@ -2,22 +2,12 @@ import React from 'react'
 import Modal from '../abstract/modal'
 import ConfigureButton from '../abstract/configureButton'
 import TabsAndPanels from '../abstract/tabsAndPanels'
-import MediaNavigator from '../navigators/mediaNavigator'
-import PageNavigator from '../navigators/pageNavigator'
 
 export default function ControlModal(props) {
     //
     const [show, setShow] = React.useState(false)
     //
     const tabs = [
-        {
-            name: "Pages",
-            content: <PageNavigator />
-        },
-        {
-            name: "Media",
-            content: <MediaNavigator />
-        },
         {
             name: "Settings",
             content: <p>Hello World.</p>
